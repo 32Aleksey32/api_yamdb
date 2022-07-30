2cb8c7ab6a1b3babd97351d49c6b47db5a6b5ce1
Алексей Осипов <a.l.e.k.s.e.y.2011@mail.ru>
2022-07-30 12:34:18 +0300

    update permissions

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -19,12 +19,14 @@ class IsAuthorOrAdmin(permissions.BasePermission):
 
     def has_permission(self, request, view):
         return (
-                request.method in permissions.SAFE_METHODS
-                or request.user.is_authenticated
-            )
+            request.method in permissions.SAFE_METHODS
+            or request.user.is_authenticated
+        )
 
     def has_object_permission(self, request, view, obj):
         return (
-                obj.author == request.user or request.user.is_admin or
-                request.user.is_moderator or request.user.is_superuser
+            obj.author == request.user
+            or request.user.is_admin
+            or request.user.is_moderator
+            or request.user.is_superuser
         )
