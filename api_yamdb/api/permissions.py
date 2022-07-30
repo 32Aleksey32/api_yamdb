1b594d6f4f9b7ebd94b7053934139c3d8a3e552f
Алексей Осипов <102141822+32Aleksey32@users.noreply.github.com>
2022-07-30 12:12:55 +0300

    Update permissions.py

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -24,7 +24,6 @@ class IsAuthorOrAdmin(permissions.BasePermission):
         )
 
     def has_object_permission(self, request, view, obj):
-        return (
-            obj.author == request.user or request.user.is_admin or
-            request.user.is_moderator or request.user.is_superuser
-        )
+        return (obj.author == request.user or request.user.is_admin or
+                request.user.is_moderator or request.user.is_superuser
+               )
