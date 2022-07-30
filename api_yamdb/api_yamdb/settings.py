13ab741bd33b197d8db15e7d140a69daf9ec3f79
Алексей Осипов <a.l.e.k.s.e.y.2011@mail.ru>
2022-07-30 13:54:08 +0300

    update settings

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -18,7 +18,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    # 'rest_framework_simplejwt',
+    'rest_framework_simplejwt',
     'django_filters',
     'reviews',
     'api',
